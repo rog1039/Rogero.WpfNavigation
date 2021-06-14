1762149bbbbd5714c23a9f2562c117fad332d598
Paul Rogero <progero@gmail.com>
2021-06-14 07:45:27 -0400

    Added better performance monitoring to RouteWorkflow.

---
+++ src/Rogero.WpfNavigation/Rogero.WpfNavigation/PerformanceTimer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog;
+
+namespace Rogero.WpfNavigation
+{
+    internal static class PerformanceTimer
+    {
+        public static PerformanceTimerManager Start(this ILogger logger, string operationName)
+        {
+            var performanceTimerManager =  new PerformanceTimerManager(logger, operationName);
+            performanceTimerManager.Start();
+            return performanceTimerManager;
+        }
+
+        private static PerformanceTimerManager StartTimer(this ILogger logger, string operationName)
+        {
+            return new PerformanceTimerManager(logger, operationName);
+        }
+
+        public sealed class PerformanceTimerManager : IDisposable
+        {
+            private readonly ILogger _logger;
+            private readonly string _operationName;
+            private readonly Guid _perfTimerId = Guid.NewGuid();
+
+            private StopwatchTimestampCheckpoint _originCheckpoint;
+            private StopwatchTimestampCheckpoint _lastCheckpoint;
+            private readonly IList<StopwatchTimestampCheckpoint> _checkpoints = new List<StopwatchTimestampCheckpoint>();
+
+            public PerformanceTimerManager(ILogger logger, string operationName)
+            {
+                _operationName = operationName;
+                _logger = logger
+                    .ForContext("Operation", operationName)
+                    .ForContext("PerfTimerId", _perfTimerId);
+            }
+
+            public void Start()
+            {
+                _originCheckpoint = _lastCheckpoint = StopwatchTimestampCheckpoint.Origin();
+                _checkpoints.Add(_originCheckpoint);
+                _logger.Information($"Starting operation: {_operationName}");
+            }
+
+            public void Checkpoint(string checkpointName)
+            {
+                if (_originCheckpoint == null) throw new InvalidOperationException("Origin checkpoint is null. Did you make a call to Start() first?");
+                
+                CheckpointInternal(checkpointName);
+                
+                _logger
+                    .Information(
+                        "Checkpoint: {CheckpointName}: Total elapsed: {TotalElapsed} [Since last checkpoint: {TimeSinceLastCheckpoint}]",
+                        checkpointName,
+                        _lastCheckpoint.ElapsedSinceRootTimeSpan,
+                        _lastCheckpoint.ElapsedSinceParentTimeSpan);
+            }
+
+            private void CheckpointInternal(string checkpointName)
+            {
+                _lastCheckpoint = _lastCheckpoint.Checkpoint(checkpointName);
+                _checkpoints.Add(_lastCheckpoint);
+            }
+
+            public IDisposable TimeSubOperation(string subOperationName)
+            {
+                var timer = new PerformanceTimerManager(_logger, subOperationName);
+                timer.Start();
+                timer.Checkpoint($"Starting {subOperationName}");
+                return timer;
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    Checkpoint($"Finished");
+                    WriteStopwatchTableResultToLog();
+                }
+            }
+
+            private void WriteStopwatchTableResultToLog()
+            {
+                var table = StopwatchTimestampCheckpoint.CheckpointsToString(_checkpoints);
+                _logger
+                    .ForContext("StopwatchTable", table)
+                    .Information(
+                        $"Total Time: {{Elapsed}}  stopwatch table for operation, {{OperationName}}:\r\n{table}",
+                                 _lastCheckpoint.ElapsedSinceRootTimeSpan,
+                                 _operationName);
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+            
+            // public void Checkpoint<T1>(string checkpointName, T1 property)
+            // {
+            //     if (_originCheckpoint == null) throw new InvalidOperationException("Origin checkpoint is null. Did you make a call to Start() first?");
+            //     
+            //     CheckpointInternal(checkpointName);
+            //     
+            //     _logger
+            //         .Information(
+            //             "Checkpoint: {CheckpointName}: Total elapsed: {TotalElapsed} [Since last checkpoint: {TimeSinceLastCheckpoint}]",
+            //             checkpointName,
+            //             _lastCheckpoint.ElapsedSinceRootTimeSpan,
+            //             _lastCheckpoint.ElapsedSinceParentTimeSpan);
+            // }
+
+            public void Checkpoint(string checkpointName, string message, params object[] items)
+            {
+                if (_originCheckpoint == null) throw new InvalidOperationException("Origin checkpoint is null. Did you make a call to Start() first?");
+                
+                CheckpointInternal(checkpointName);
+
+                var paramCount = 3 + items.Length;
+                var logParams  = new object[paramCount];
+                logParams[0] = checkpointName;
+                logParams[1] = _lastCheckpoint.ElapsedSinceRootTimeSpan;
+                logParams[2] = _lastCheckpoint.ElapsedSinceParentTimeSpan;
+                for (int i = 0; i < items.Length; i++)
+                {
+                    logParams[i + 3] = items[i];
+                }
+                
+                
+                _logger
+                    .Information(
+                        "Checkpoint: {CheckpointName}: Total elapsed: {TotalElapsed} [Since last checkpoint: {TimeSinceLastCheckpoint}] " + message,
+                        logParams
+                        );
+            }
+
+        }
+    }
+}
--- src/Rogero.WpfNavigation/Rogero.WpfNavigation/RouteWorkflowTask.cs
+++ src/Rogero.WpfNavigation/Rogero.WpfNavigation/RouteWorkflowTask.cs
@@ -12,119 +12,130 @@ namespace Rogero.WpfNavigation
     public class RouteWorkflowTask
     {
         public static RouteWorkflowTask Go(
-            RouteRequest               routeRequest,
-            IRouteEntryRegistry        routeEntryRegistry,
+            RouteRequest routeRequest,
+            IRouteEntryRegistry routeEntryRegistry,
             IRouteAuthorizationManager routeAuthorizationManager,
-            IRouterService             routerService,
-            ILogger                    logger)
+            IRouterService routerService,
+            ILogger logger)
         {
             var workflow = new RouteWorkflowTask(routeRequest, routeEntryRegistry, routeAuthorizationManager,
-                                                 routerService,
-                                                 logger);
+                routerService,
+                logger);
             workflow.RouteResult = workflow.GoAsync();
             return workflow;
         }
 
-        public string          Uri             => _routeRequest.Uri;
-        public object          InitData        => _routeRequest.InitData;
+        public string Uri => _routeRequest.Uri;
+        public object InitData => _routeRequest.InitData;
         public ViewportOptions ViewportOptions => _routeRequest.ViewportOptions;
-        public Guid            RouteRequestId  => _routeRequest.RouteRequestId;
-        public Option<string>  RouteName       => RouteEntryOption.Select(routeEntry => routeEntry.Name);
+        public Guid RouteRequestId => _routeRequest.RouteRequestId;
+        public Option<string> RouteName => RouteEntryOption.Select(routeEntry => routeEntry.Name);
 
-        public Guid                RoutingWorkflowId { get; } = Guid.NewGuid();
-        public DateTime            StartedTime       { get; } = DateTime.UtcNow;
-        public DateTime            FinishedTime      { get; private set; }
-        public Option<IRouteEntry> RouteEntryOption  { get; private set; }
-        public Task<RouteResult>   RouteResult       { get; private set; }
+        public Guid RoutingWorkflowId { get; } = Guid.NewGuid();
+        public DateTime StartedTime { get; } = DateTime.UtcNow;
+        public DateTime FinishedTime { get; private set; }
+        public Option<IRouteEntry> RouteEntryOption { get; private set; }
+        public Task<RouteResult> RouteResult { get; private set; }
 
-        public object    Controller { get; private set; }
-        public UIElement View       { get; private set; }
+        public object Controller { get; private set; }
+        public UIElement View { get; private set; }
 
-        private readonly ILogger                    _logger;
-        private readonly IRouteEntryRegistry        _routeEntryRegistry;
+        private readonly ILogger _logger;
+        private readonly IRouteEntryRegistry _routeEntryRegistry;
         private readonly IRouteAuthorizationManager _routeAuthorizationManager;
-        private readonly IRouterService             _routerService;
+        private readonly IRouterService _routerService;
 
         private readonly RouteRequest _routeRequest;
 
         internal RouteWorkflowTask(
-            RouteRequest               routeRequest,
-            IRouteEntryRegistry        routeEntryRegistry,
+            RouteRequest routeRequest,
+            IRouteEntryRegistry routeEntryRegistry,
             IRouteAuthorizationManager routeAuthorizationManager,
-            IRouterService             routerService,
-            ILogger                    logger)
+            IRouterService routerService,
+            ILogger logger)
         {
             _routeRequest = routeRequest;
 
-            _routeEntryRegistry        = routeEntryRegistry;
+            _routeEntryRegistry = routeEntryRegistry;
             _routeAuthorizationManager = routeAuthorizationManager;
-            _routerService             = routerService;
+            _routerService = routerService;
             _logger = logger
-                .ForContext("Class",             nameof(RouteWorkflowTask))
-                .ForContext("Uri",               Uri)
-                .ForContext("ViewportName",      ViewportOptions.ToString())
-                .ForContext("InitData",          InitData)
-                .ForContext("RouterServiceId",   _routerService.RouterServiceId)
+                .ForContext("Class", nameof(RouteWorkflowTask))
+                .ForContext("Uri", Uri)
+                .ForContext("ViewportName", ViewportOptions.ToString())
+                .ForContext("InitData", InitData)
+                .ForContext("RouterServiceId", _routerService.RouterServiceId)
                 .ForContext("RoutingWorkflowId", RoutingWorkflowId);
         }
 
         internal async Task<RouteResult> GoAsync()
         {
-            var initDataIsNull = InitData != null ? "with init data" : "without init data";
-            using (Logging.Timing(
-                _logger, $"navigation workflow URI: [{Uri}], in viewport [{ViewportOptions}] " + initDataIsNull)
-            )
+            var initDataIsNull = InitData != null ? " with init data" : " without init data";
+            using var timer = PerformanceTimer.Start(_logger,
+                $"Routing operation to {Uri} in Viewport {ViewportOptions.ToString() + initDataIsNull}");
+
+            try
             {
-                try
-                {
-                    RouteEntryOption = RouteWorkflow.GetRouteEntry(_logger, _routeEntryRegistry, Uri);
+                RouteEntryOption = RouteWorkflow.GetRouteEntry(_logger, _routeEntryRegistry, Uri);
+                timer.Checkpoint("Route entry retrieved.");
 
-                    var routeResult = await RouteEntryOption.Match(
-                        some: async routeEntry =>
+                var routeResult = await RouteEntryOption.Match(
+                    some: async routeEntry =>
+                    {
+                        //Check authorization
+                        var routeContext = new RoutingContext(routeEntry, _routeRequest);
+                        var authorized =
+                            await RouteWorkflow.CheckRouteAuthorizationAsync(
+                                _logger, _routeAuthorizationManager, routeContext);
+
+                        timer.Checkpoint("Route auth checked.");
+                        if (!authorized) return new RouteResult(RouteResultStatusCode.Unauthorized);
+
+                        //Can deactivate current
+                        var canDeactivate =
+                            await RouteWorkflow.CanDeactivateCurrentRouteAsync(
+                                _logger, _routerService, ViewportOptions, Uri,
+                                InitData);
+                        timer.Checkpoint("CanDeactivate checked.");
+                        if (!canDeactivate) return new RouteResult(RouteResultStatusCode.CanDeactiveFailed);
+
+                        //Activate new route
+                        var canActivate = await RouteWorkflow.CanActivateNewRouteAsync(_logger);
+                        timer.Checkpoint("Activate checked.");
+                        if (!canActivate) return new RouteResult(RouteResultStatusCode.CanActivateFailed);
+
+                        //Get ViewModel
+                        Controller = RouteWorkflow.CreateViewModel(_logger, routeEntry);
+                        timer.Checkpoint("Viewmodel created");
+                        await RouteWorkflow.InitViewModel(_logger, InitData, Controller);
+                        timer.Checkpoint("Viewmodel inited.");
+                        //Get View
+                        View = RouteWorkflow.CreateView(_logger, routeEntry);
+                        timer.Checkpoint("View created.");
+                        RouteWorkflow.AssignDataContext(_logger, View, Controller);
+
+                        //IViewAware context
+                        if (Controller is IViewAware viewAware)
                         {
-                            //Check authorization
-                            var routeContext = new RoutingContext(routeEntry, _routeRequest);
-                            var authorized =
-                                await RouteWorkflow.CheckRouteAuthorizationAsync(
-                                    _logger, _routeAuthorizationManager, routeContext);
-                            if (!authorized) return new RouteResult(RouteResultStatusCode.Unauthorized);
+                            RouteWorkflow.AssignViewToViewModel(_logger, View, viewAware);
+                            timer.Checkpoint("ViewModel is view aware, so view assigned to view model");
+                        }
 
-                            //Can deactivate current
-                            var canDeactivate =
-                                await RouteWorkflow.CanDeactivateCurrentRouteAsync(
-                                    _logger, _routerService, ViewportOptions, Uri,
-                                    InitData);
-                            if (!canDeactivate) return new RouteResult(RouteResultStatusCode.CanDeactiveFailed);
+                        //Add View to UI
+                        var routeResult = RouteWorkflow.AddViewToUi(_logger, _routerService, this, View);
+                        timer.Checkpoint("View added to UI.K");
+                        return routeResult;
+                    },
+                    none: async () => new RouteResult(RouteResultStatusCode.RouteNotFound));
 
-                            //Activate new route
-                            var canActivate = await RouteWorkflow.CanActivateNewRouteAsync(_logger);
-                            if (!canActivate) return new RouteResult(RouteResultStatusCode.CanActivateFailed);
+                FinishedTime = DateTime.UtcNow;
 
-                            //Get ViewModel
-                            Controller = RouteWorkflow.CreateViewModel(_logger, routeEntry);
-                            await RouteWorkflow.InitViewModel(_logger, InitData, Controller);
-                            //Get View
-                            View = RouteWorkflow.CreateView(_logger, routeEntry);
-                            RouteWorkflow.AssignDataContext(_logger, View, Controller);
-
-                            //IViewAware context
-                            if (Controller is IViewAware viewAware)
-                                RouteWorkflow.AssignViewToViewModel(_logger, View, viewAware);
-
-                            //Add View to UI
-                            return RouteWorkflow.AddViewToUi(_logger, _routerService, this, View);
-                        },
-                        none: async () => new RouteResult(RouteResultStatusCode.RouteNotFound));
-
-                    FinishedTime = DateTime.UtcNow;
-
-                    return routeResult;
-                }
-                catch (Exception e)
-                {
-                    _logger.Error(e, "Exception during RouteWorkflowTask.Go() method.");
-                    throw;
-                }
+                return routeResult;
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Exception during RouteWorkflowTask.Go() method.");
+                throw;
             }
         }
 
@@ -133,15 +144,15 @@ namespace Rogero.WpfNavigation
             return $"{RouteName}: {Uri} -> {ViewportOptions}";
         }
 
-        private void LogInfo(string        message)                   => _logger.Information(message);
-        private void LogInfo<T>(string     message, T data)           => _logger.Information(message, data);
+        private void LogInfo(string message) => _logger.Information(message);
+        private void LogInfo<T>(string message, T data) => _logger.Information(message, data);
         private void LogInfo<T, T1>(string message, T data, T1 data1) => _logger.Information(message, data, data1);
 
         private void LogInfo<T, T1, T2>(string message, T data, T1 data1, T2 data2) =>
             _logger.Information(message, data, data1, data2);
 
-        private void LogWarning(string        message)                   => _logger.Warning(message);
-        private void LogWarning<T>(string     message, T data)           => _logger.Warning(message, data);
+        private void LogWarning(string message) => _logger.Warning(message);
+        private void LogWarning<T>(string message, T data) => _logger.Warning(message, data);
         private void LogWarning<T, T1>(string message, T data, T1 data1) => _logger.Warning(message, data, data1);
 
         private void LogError<T>(string message, T data) => _logger.Error((Exception) null, message, data);
